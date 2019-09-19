5206471dd17dc457866838e1c4466d3b3a6f41bd
Robin Harms Oredsson <robin@betahaus.net>
2019-09-19 09:53:05 +0200

    Convert roles to str

--- src/kedja/models/json.py
+++ src/kedja/models/json.py
@@ -3,11 +3,18 @@ from datetime import datetime
 from pyramid.interfaces import IRendererFactory
 
 
-def datetime_adapter(obj: datetime, request):
+def datetime_adapter(obj, request):
     return obj.isoformat()
 
 
+def roles_adapter(obj, request):
+    return str(obj)
+
+
 def includeme(config):
-    """ Include rendering for datetime objects. """
+    """ Include rendering special objects. """
+    from kedja.models.acl import Role
+
     json_renderer = config.registry.getUtility(IRendererFactory, name="json")
     json_renderer.add_adapter(datetime, datetime_adapter)
+    json_renderer.add_adapter(Role, roles_adapter)
